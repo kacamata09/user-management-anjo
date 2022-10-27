ae6e18c29ae53d12b6196e89fbd7ba7cd266e90c
kacamata09 <muh.ansharazhari@gmail.com>
2022-10-27 09:57:22 +0700

    ubah dashboard user, tambahin selamat datang

--- controller/index-controller.js
+++ controller/index-controller.js
@@ -1,7 +1,14 @@
+const koneksi = require('../config/database')
 module.exports = {
     tampil(requ, resp) {
         requ.session.loggedid = true
         id = requ.session.userid
-        resp.render('dashboard_user.ejs', idUser = {id})
+        const ambilUser = 'select * from pengguna where id = ?'
+        koneksi.query(ambilUser, id, (err, rows, field) => {
+            if (err) throw err
+            const user = rows[0]
+            resp.render('dashboard_user.ejs', idUser = {id, user})
+        })
+
     }
 }
