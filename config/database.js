dc1680ffd54cf00a6d04513fd961aa0eaf734fda
johanespratama9 <johanespratama09@gmail.com>
2022-10-27 16:52:15 +0700

    konfirmasi hapus data user

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const mysql = require('mysql')
 const koneksi = mysql.createConnection({
     host: 'localhost',
     user:'root',
-    password:'',
+    password:'PASSWORD_HERE',
     database:'dbcoba'
 })
 
