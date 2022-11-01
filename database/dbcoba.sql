03fcccba90dd5ed928bf99e510e0698e60db8706
kacamata09 <muh.ansharazhari@gmail.com>
2022-11-01 17:00:35 +0700

    tambahin nama pada konfirmasi hapus

--- database/dbcoba.sql
+++ database/dbcoba.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 01, 2022 at 04:20 AM
+-- Generation Time: Nov 01, 2022 at 10:02 AM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 7.4.15
 
@@ -40,6 +40,10 @@ INSERT INTO `cek_email` (`email`) VALUES
 ('aaaa'),
 ('aaaa'),
 ('aaaa'),
+('aaaa'),
+('aaaa'),
+('aaaa'),
+('aaaa'),
 ('aaaa');
 
 -- --------------------------------------------------------
@@ -62,16 +66,10 @@ CREATE TABLE `pengguna` (
 --
 
 INSERT INTO `pengguna` (`id`, `nama`, `email`, `role`, `password`, `status`) VALUES
-(1, 'anshar admin', 'admin@admin.com', 'admin', '3c9909afec25354d551dae21590bb26e38d53f2173b8d3dc3eee4c047e7ab1c1eb8b85103e3be7ba613b31bb5c9c36214dc9f14a42fd7a2fdb84856bca5c44c2', 'aktif'),
-(15, 'user1234', 'user@user.com', 'user', '5aadb45520dcd8726b2822a7a78bb53d794f557199d5d4abdedd2c55a4bd6ca73607605c558de3db80c8e86c3196484566163ed1327e82e8b6757d1932113cb8', 'aktif'),
-(16, 'anshar', 'muh.ansharibrahim@gmail.com', 'user', '3c9909afec25354d551dae21590bb26e38d53f2173b8d3dc3eee4c047e7ab1c1eb8b85103e3be7ba613b31bb5c9c36214dc9f14a42fd7a2fdb84856bca5c44c2', 'aktif'),
-(17, 'johan', 'johanespratama09@gmail.com', 'user', '3c9909afec25354d551dae21590bb26e38d53f2173b8d3dc3eee4c047e7ab1c1eb8b85103e3be7ba613b31bb5c9c36214dc9f14a42fd7a2fdb84856bca5c44c2', 'blokir'),
-(23, 'coba ubah 2', 'cob@a.com222', 'user', '3c9909afec25354d551dae21590bb26e38d53f2173b8d3dc3eee4c047e7ab1c1eb8b85103e3be7ba613b31bb5c9c36214dc9f14a42fd7a2fdb84856bca5c44c2', 'aman'),
-(26, 'coba', 'asas@asa', 'user', '3c9909afec25354d551dae21590bb26e38d53f2173b8d3dc3eee4c047e7ab1c1eb8b85103e3be7ba613b31bb5c9c36214dc9f14a42fd7a2fdb84856bca5c44c2', 'aman'),
-(27, 'anshar', 'an@ta', 'user', 'fa585d89c851dd338a70dcf535aa2a92fee7836dd6aff1226583e88e0996293f16bc009c652826e0fc5c706695a03cddce372f139eff4d13959da6f1f5d3eabe', 'aktif'),
-(28, 'aa', 'asada@sdsd.coms', 'user', 'fa585d89c851dd338a70dcf535aa2a92fee7836dd6aff1226583e88e0996293f16bc009c652826e0fc5c706695a03cddce372f139eff4d13959da6f1f5d3eabe', 'aman'),
-(29, 'cobas', '21@a', 'user', 'fa585d89c851dd338a70dcf535aa2a92fee7836dd6aff1226583e88e0996293f16bc009c652826e0fc5c706695a03cddce372f139eff4d13959da6f1f5d3eabe', 'aman'),
-(30, 'dsd', '22@s', 'user', 'fa585d89c851dd338a70dcf535aa2a92fee7836dd6aff1226583e88e0996293f16bc009c652826e0fc5c706695a03cddce372f139eff4d13959da6f1f5d3eabe', 'aman');
+(39, 'coba', '123@aa.com', 'user', '$2b$10$LdVwOKHKOuKxZrNaodll..m/imkzvF7Jv8JoUXW.e9wDUGCSo08d6', 'aktif'),
+(40, 'admin', 'admin@admin.com', 'admin', '$2b$10$WoJ.5MkfUsmGEfnpnOB6IuB0mHX1UHjzw6ygDSSUkFqK.9vZACKZW', 'aktif'),
+(41, 'anshard', 'muh.ansharibrahim@gmail.com', 'admin', '$2b$10$1KX0BJUsM4yRSIHa8ua6w.E7RvLIdwjRnM4UC8z1utI13yTUIzUle', 'aktif'),
+(42, 'a', 'an@t.com', 'user', '$2b$10$NNFpcDwWPQuVOZbSxr8FmuR9vR8XXG4E.a2f/oV3EyO9ZpooojCo6', 'aktif');
 
 -- --------------------------------------------------------
 
@@ -110,7 +108,7 @@ ALTER TABLE `pengguna`
 -- AUTO_INCREMENT for table `pengguna`
 --
 ALTER TABLE `pengguna`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
