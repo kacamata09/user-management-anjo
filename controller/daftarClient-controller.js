1b5b68d0a170e033122e669087e31feec333b491
kacamata09 <muh.ansharazhari@gmail.com>
2022-11-07 11:05:44 +0700

    selsaikan edit hapus client

--- controller/daftarClient-controller.js
+++ controller/daftarClient-controller.js
@@ -17,8 +17,16 @@ function randomString(length) {
 
 module.exports = {
   async tampilDaftarClient(requ, resp) {
+    koneksi.query('select * from clientconfig', (err, rows, field) => {
+      if (err) throw err
+
+      if(rows.length > 0) {
+        resp.render('tampil_client.ejs', clients=rows)
+        return
+      }
+    })
     
-    resp.render('tampil_client.ejs')
+    // resp.render('tampil_client.ejs')
   },
   tambahClient(requ, resp) {
 
@@ -38,7 +46,21 @@ module.exports = {
     const client_id = requ.params.client_id
     const namaApp = requ.body.namaApp
     const redirect_uri = requ.body.redirect_uri
-    koneksi.query('update clientconfig ')
+    koneksi.query('update clientconfig set nama_client = ?, redirect_uri = ? where client_id = ?', [namaApp, redirect_uri, client_id], (err, rows, field) => {
+      resp.redirect('/client')
+      return
+    })
+
+  },
+  hapusClient(requ, resp) {
+    const client_id = requ.params.client_id
+    koneksi.query('delete from clientconfig where client_id = ?', client_id, (err, rows, field) => {
+      resp.redirect('/client')
+      return
+    })
+  },
+  tampilEdit(requ, resp) {
+    resp.render('edit_client.ejs')
   }
   
 
--- routes/daftarClient-routes.js
+++ routes/daftarClient-routes.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const clientController = require('../controller/client-controller')
 const client_controller = require('../controller/daftarClient-controller')
 const oidc_controller = require('../controller/oidc-controller')
 
@@ -9,7 +10,10 @@ clientRouter.route('/client')
     .get(client_controller.tampilDaftarClient)
     .post(client_controller.tambahClient)
 
-clientRouter.get('/client/edit/:client_id')
+clientRouter.get('/client/edit/:client_id', client_controller.tampilEdit)
+clientRouter.post('/client/edit/:client_id', client_controller.editClient)
+clientRouter.get('/client/hapus/:client_id', client_controller.hapusClient)
+
 
 
 
