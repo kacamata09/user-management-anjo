7a4f1e53ff0832238ffccbd18245cc816dc61843
johanespratama9 <johanespratama09@gmail.com>
2022-11-09 10:07:50 +0700

    Update daftarClient-routes.js

--- routes/daftarClient-routes.js
+++ routes/daftarClient-routes.js
@@ -2,6 +2,8 @@ const express = require('express')
 const clientController = require('../controller/client-controller')
 const client_controller = require('../controller/daftarClient-controller')
 const oidc_controller = require('../controller/oidc-controller')
+const midwareIngatSaya = require('../config/verifIngat')
+const midwwaraVerifAdmin = require('../config/verifyAdmin')
 const multer = require('multer')
 const clientRouter = express.Router()
 
@@ -20,12 +22,12 @@ const upload = multer({storage:penyimpanan})
 
 
 clientRouter.route('/client')
-    .get(client_controller.tampilDaftarClient)
+    .get([midwareIngatSaya.ingatSaya,midwwaraVerifAdmin.isAdmin],client_controller.tampilDaftarClient)
     .post(upload.single('logo_aplikasi'), client_controller.tambahClient)
 
-clientRouter.get('/client/edit/:client_id', client_controller.tampilEdit)
-clientRouter.post('/client/edit/:client_id', client_controller.editClient)
-clientRouter.get('/client/hapus/:client_id', client_controller.hapusClient)
+clientRouter.get('/client/edit/:client_id',midwwaraVerifAdmin.isAdmin, client_controller.tampilEdit)
+clientRouter.post('/client/edit/:client_id',midwwaraVerifAdmin.isAdmin, client_controller.editClient)
+clientRouter.get('/client/hapus/:client_id',midwwaraVerifAdmin.isAdmin, client_controller.hapusClient)
 
 
 
